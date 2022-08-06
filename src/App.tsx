cabdbc410cbe877c1ce1f24f2ee7ad14b248b7a3
Raphael Rocha <raphaelfsrocha@gmail.com>
2022-08-06 09:52:18 -0300

    progressstepmap

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
 import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {} from '@fortawesome/fontawesome-svg-core'
-import {
-  faArrowRightLong,
-  faCheck,
-  faCoffee,
-} from '@fortawesome/free-solid-svg-icons'
 import arrowGrey from './assets/svg/arrowGrey.svg'
 import arrowPurple from './assets/svg/arrowPurple.svg'
 import checkStep from './assets/svg/checkStep.svg'
-import littleArrowGrey from './assets/svg/littleArrowGrey.svg'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+
+const progressSteps = [
+  { id: 1, name: 'step1' },
+  { id: 2, name: 'step2' },
+  { id: 3, name: 'step3' },
+  { id: 4, name: 'step4' },
+]
 
 function App() {
   const [arrow, setArrow] = useState(true)
   const [check, setCheck] = useState(true)
   const [count, setCount] = useState(0)
   const [productChoice, setProductChoice] = useState(false)
+  const [step, setStep] = useState(0)
+
+  const handleChangeStep = (e: any) => {
+    e.target.value === 'prev' && setStep(step - 1)
+    e.target.value === 'next' &&
+      step < progressSteps.length &&
+      setStep(step + 1)
+  }
 
   return (
     <>
       <div className='grid-step'>
         <div className='step-product'>Produto</div>
+        
         <div className='circle-step '>
-          <img
-            src={!check ? checkStep : ''}
-            alt='Check'
-            className='check-style'
-          />
+          <img src={!check ? checkStep : ''} alt='' className='check-style' />
         </div>
         <div className='step-color'>Cor</div>
-        <div className='circle-step circle2'></div>
-        <div className='circle-step circle3'></div>
-        <div className='circle-step circle4'></div>
+        <div className='circle-step circle2'>
+          <img src={!check ? checkStep : ''} alt='' className='check-style' />
+        </div>
+        <div className='circle-step circle3'>
+          <img src={!check ? checkStep : ''} alt='' className='check-style' />
+        </div>
+        <div className='circle-step circle4'>
+          <img src={!check ? checkStep : ''} alt='' className='check-style' />
+        </div>
         <div className='step-accessory'>Acessórios</div>
         <div className='step-review'>Revisão</div>
         <div className='step-user-product'>
@@ -54,7 +64,7 @@ function App() {
         </div>
         <div className='arrow-step3'>
           <img
-            src={arrowGrey}
+            src={arrow ? arrowGrey : arrowPurple}
             alt='arrowright'
             className='arrow-step-img-disable'
           />
@@ -67,7 +77,7 @@ function App() {
         }}
       >
         {' '}
-        Arrow change {count}
+        Check e arrow true e false teste {count}
       </button>{' '}
       <button
         onClick={() => {
