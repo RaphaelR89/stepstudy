19964bf098c36c507bde2bb60fa9f754a8462dcd
Raphael Rocha <raphaelfsrocha@gmail.com>
2022-08-06 10:46:19 -0300

    logica map ajuda

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,20 @@ import './App.css'
 import arrowGrey from './assets/svg/arrowGrey.svg'
 import arrowPurple from './assets/svg/arrowPurple.svg'
 import checkStep from './assets/svg/checkStep.svg'
-import { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 
-const progressSteps = [
-  { id: 1, name: 'step1' },
-  { id: 2, name: 'step2' },
-  { id: 3, name: 'step3' },
-  { id: 4, name: 'step4' },
-]
+const progressSteps = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+//ta recriando 4 x a estrutura map e sobrepondo por isso n muda a cor, so no 4
 
 function App() {
   const [arrow, setArrow] = useState(true)
   const [check, setCheck] = useState(true)
   const [count, setCount] = useState(0)
   const [productChoice, setProductChoice] = useState(false)
-  const [step, setStep] = useState(0)
+  const [step, setStep] = useState(1)
 
   const handleChangeStep = (e: any) => {
-    e.target.value === 'prev' && setStep(step - 1)
+    e.target.value === 'prev' && step > 0 && setStep(step - 1)
     e.target.value === 'next' &&
       step < progressSteps.length &&
       setStep(step + 1)
@@ -29,64 +25,95 @@ function App() {
     <>
       <div className='grid-step'>
         <div className='step-product'>Produto</div>
-        
-        <div className='circle-step '>
-          <img src={!check ? checkStep : ''} alt='' className='check-style' />
-        </div>
-        <div className='step-color'>Cor</div>
-        <div className='circle-step circle2'>
-          <img src={!check ? checkStep : ''} alt='' className='check-style' />
-        </div>
-        <div className='circle-step circle3'>
-          <img src={!check ? checkStep : ''} alt='' className='check-style' />
-        </div>
-        <div className='circle-step circle4'>
-          <img src={!check ? checkStep : ''} alt='' className='check-style' />
-        </div>
-        <div className='step-accessory'>Acessórios</div>
-        <div className='step-review'>Revisão</div>
-        <div className='step-user-product'>
-          {productChoice ? 'notebook' : 'product'}
-        </div>
-        <div className='arrow-step'>
-          <img
-            src={arrow ? arrowGrey : arrowPurple}
-            alt='arrowright'
-            className='arrow-step-img-disable'
-          />
-        </div>
-        <div className='arrow-step2'>
-          <img
-            src={arrow ? arrowGrey : arrowPurple}
-            alt='arrowright'
-            className='arrow-step-img-disable'
-          />
-        </div>
-        <div className='arrow-step3'>
-          <img
-            src={arrow ? arrowGrey : arrowPurple}
-            alt='arrowright'
-            className='arrow-step-img-disable'
-          />
-        </div>
+        {progressSteps.map((_, index) => (
+          <React.Fragment key={index}>
+            {index <= step ? (
+              index === step ? (
+                <>
+                  <div className='circle-step '>
+                    <img src='' alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step'>
+                    <img
+                      src={arrow ? arrowGrey : arrowPurple}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div id='2' className='circle-step circle2'>
+                    <img src='' alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step2'>
+                    <img
+                      src={arrowGrey}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div id='3' className='circle-step circle3'>
+                    <img src='' alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step3'>
+                    <img
+                      src={arrowGrey}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div id='4' className='circle-step circle4'>
+                    <img src='' alt='' className='check-style' />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className='circle-step '>
+                    <img src={checkStep} alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step'>
+                    <img
+                      src={arrowPurple}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div className='circle-step circle2 '>
+                    <img src={checkStep} alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step2'>
+                    <img
+                      src={arrowPurple}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div className='circle-step circle3'>
+                    <img src={checkStep} alt='' className='check-style' />
+                  </div>
+                  <div className='arrow-step3'>
+                    <img
+                      src={arrowPurple}
+                      alt='arrowright'
+                      className='arrow-step-img-disable'
+                    />
+                  </div>
+                  <div className='circle-step circle4'>
+                    <img src={checkStep} alt='' className='check-style ' />
+                  </div>
+                </>
+              )
+            ) : null}
+          </React.Fragment>
+        ))}
+        <hr />
+        Step:{step}
+        <br />
+        <button value='prev' onClick={handleChangeStep}>
+          Previous
+        </button>
+        <button value='next' onClick={handleChangeStep}>
+          Next Step
+        </button>
       </div>
-      <button
-        onClick={() => {
-          setArrow(!arrow)
-          setCheck(!check)
-        }}
-      >
-        {' '}
-        Check e arrow true e false teste {count}
-      </button>{' '}
-      <button
-        onClick={() => {
-          setProductChoice(!productChoice)
-        }}
-      >
-        {' '}
-        Product change
-      </button>{' '}
     </>
   )
 }
